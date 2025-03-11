326c6aede782c3e5a1947998fdf7606695e7ba94
Amparo Oliver <70351967+amparooliver@users.noreply.github.com>
2025-03-11 08:01:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ args = dotdict({
     'tempThreshold': 30,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 2000, ## Number of games moves for MCTS to simulate. 18496
+    'numMCTSSims': 1000, ## Number of games moves for MCTS to simulate. 18496
     'arenaCompare': 20,
     'cpuct': 1,
 
